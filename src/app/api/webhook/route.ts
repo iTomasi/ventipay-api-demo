a84bfebbfe0c648fbe5ca51233d5e05f7d00b62d
Tomas Duclos <duclosiw@gmail.com>
2024-01-01 17:10:51 -0300

    webhook change

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -5,11 +5,16 @@ import { EmailPurchase } from '@/components/emails'
 import { render } from '@react-email/render'
 
 export const POST = async (req: Request): Promise<Response> => {
+  const ventiSignature = req.headers.get('venti-signature')
+
+  if (ventiSignature === null || ventiSignature === '') {
+    return NextResponse.json({ message: 'Nope' }, { status: 401 })
+  }
+
+  // TODO: signature verification
+  console.log(ventiSignature)
+
   const json = await req.json()
-
-  // const ventiSignature = req.headers.get('venti-signature')
-
-  // console.log(ventiSignature)
 
   if (json.type !== 'checkout.paid') return NextResponse.json({ success: true })
 
