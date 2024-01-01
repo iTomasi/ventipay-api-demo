3d22e9a724b856c3b349cdeec85230246ad03784
Tomas Duclos <duclosiw@gmail.com>
2023-12-31 23:08:17 -0300

    removing console.log

--- src/server/FetchBase.ts
+++ src/server/FetchBase.ts
@@ -3,7 +3,6 @@ import { VENTIPAY_API_KEY_SECRET } from './envs'
 const VENTIPAY_BASIC_AUTHORIZATION = btoa(VENTIPAY_API_KEY_SECRET + ':' + '')
 
 export const FetchVentiPay = async (pathname: string, init?: RequestInit): Promise<Response> => {
-  console.log(VENTIPAY_API_KEY_SECRET)
   const fetching = await fetch(
     'https://api.ventipay.com/v1' + pathname,
     {
